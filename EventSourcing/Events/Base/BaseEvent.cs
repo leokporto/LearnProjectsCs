78b4734aae9b8d4b12b5d06d5732f4c469ac286b
Leonardo Porto <leokporto@gmail.com>
2024-05-28 08:04:30 -0300

    Added eventsourcing sample

---
+++ EventSourcing/Events/Base/BaseEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSourcing.Events.Base
+{
+	public abstract class BaseEvent
+	{
+		public abstract Guid StreamId { get; }
+
+		public long CreatedAtUtcTicks { get; set; }
+	}
+}
